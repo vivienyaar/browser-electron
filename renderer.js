91ebd30e34a69f6f559d437bca3af33695347dbe
vivienyaar <vivien@yaar.ai>
2018-01-02 10:15:30 -0500

    get elements

--- renderer.js
+++ renderer.js
@@ -1,1 +1,19 @@
+var ById = function (id) {
+	return document.getElementById(id);
+}
 
+var jsonfile = require('jsonfile');
+var favicon = require('favicon-getter').default;
+var path = require('path');
+var uuid = require('uuid');
+var bookmarks = path.join(__dirname, 'bookmarks.json');
+
+var back = ById('back');
+var forward = ById('forward');
+var refresh = ById('refresh');
+var omni = ById('url');
+var dev = ById('console');
+var fave = ById('fave');
+var list = ById('list');
+var popup = ById('fave-popup');
+var view = ById('view');
